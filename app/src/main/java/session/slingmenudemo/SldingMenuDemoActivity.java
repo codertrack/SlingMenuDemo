1a4d7d9c2041c0d70836261a99bf013fd0bffcd0
wukai <wukai@wukai.com>
2016-11-01 17:17:47 +0800

    conmimt

--- app/src/main/java/session/slingmenudemo/SldingMenuDemoActivity.java
+++ app/src/main/java/session/slingmenudemo/SldingMenuDemoActivity.java
@@ -3,6 +3,8 @@ package session.slingmenudemo;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import session.slidnglibrary.SlidingMenu;
 import session.slidnglibrary.app.SlidingFragmentActivity;
@@ -12,6 +14,7 @@ public class SldingMenuDemoActivity extends SlidingFragmentActivity {
 
 	private MenuFragment mFrag;
 
+	SlidingMenu sm=null;
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -29,15 +32,25 @@ public class SldingMenuDemoActivity extends SlidingFragmentActivity {
 
 
 		//获取SlidingMenu对象
-		SlidingMenu sm = getSlidingMenu();
+		sm = getSlidingMenu();
 		//设置阴影宽度
 		sm.setShadowWidthRes(R.dimen.shadow_width);
 		//sm.setShadowDrawable(R.drawable.shadow);
 		sm.setBehindOffsetRes(R.dimen.slidingmenu_offset);
 		sm.setFadeDegree(0.35f);
-		sm.setTouchModeAbove(SlidingMenu.TOUCHMODE_FULLSCREEN);
+
+		//全屏开始触摸
+		//sm.setTouchModeAbove(SlidingMenu.TOUCHMODE_FULLSCREEN);
+
+		//从内容区域触摸有效
+		sm.setTouchModeAbove(SlidingMenu.SLIDING_CONTENT);
 
 	}
 
+	private boolean opened = false;
 
+	public void openAndclose(View view) {
+		sm.toggle();
+		opened = !opened;
+	}
 }
